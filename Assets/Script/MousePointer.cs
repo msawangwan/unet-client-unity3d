789ab9512c9dc8ab88420ddefdef33d8ba4a44e9
msawangwan <misha.sawangwan@gmail.com>
2016-09-22 18:15:34 -0700

    started new turret

---
+++ Assets/Script/MousePointer.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public static class MousePointer {
+
+    public static Vector3 ScreenCoordinates ( float depth = 10.0f ) {
+        return Camera.main.ScreenToWorldPoint ( new Vector3 ( Input.mousePosition.x, Input.mousePosition.y, depth ) );
+    }
+}
