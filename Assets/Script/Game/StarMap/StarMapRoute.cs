01b3bf81982a4163129830bf32564a5ccffb65f8
msawangwan <misha.sawangwan@gmail.com>
2016-10-04 18:14:20 -0700

    another ninja commit

--- Assets/Script/Game/StarMap/StarMapRoute.cs
+++ Assets/Script/Game/StarMap/StarMapRoute.cs
@@ -2,9 +2,17 @@
 
 [RequireComponent(typeof(LineRenderer))]
 public class StarMapRoute : MonoBehaviour {
+    public static StarMapRoute Instance = null;
+
     private LineRenderer lr = null;
 
     void Start () {
+        Instance = CommonUtil.EnablePersistance(this, gameObject);
         lr = gameObject.GameObjectComponent<LineRenderer>();
+    }
+
+    public void DrawRoute (Vector3 s, Vector3 e) {
+        Vector3[] nodes = new Vector3[] { s, e };
+        lr.SetPositions(nodes);
     }
 }
